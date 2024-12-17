d888ab01853cfccf56b3708750020ab38892cdee
Anurag2618g <anurag2618g@gmail.com>
2024-12-17 23:37:37 +0530

    displaying owned NFTs using router

--- src/opend_assets/src/components/App.jsx
+++ src/opend_assets/src/components/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
-import homeImage from "../../assets/home-img.png";
 import Item from "./Item";
 import Minter from "./Minter";
 
@@ -15,7 +14,6 @@ function App() {
       <Header />
       <Minter />
       {/* <Item id= {NFTID} /> */}
-      {/* <img className="bottom-space" src={homeImage} /> */}
       <Footer />
     </div>
   );
--- src/opend_assets/src/components/Header.jsx
+++ src/opend_assets/src/components/Header.jsx
@@ -1,30 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../../assets/logo.png";
+import homeImage from "../../assets/home-img.png";
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import Minter from "./Minter";
+import Gallery from "./Gallery";
+import { opend } from "../../../declarations/opend";
 
 function Header() {
+
+  const [userOwnedGallery, setOwnedGallery] = useState();
+
+  async function getNFTs() {
+    const userNFTIds = await opend.getOwnedNFTs(CURRENT_USER_ID);
+    console.log(userNFTIds);
+    setOwnedGallery(<Gallery title="Mt NFTs" ids={userNFTIds} />);
+  } 
+
+  useEffect(() => {
+    getNFTs();
+  }, []);
+
   return (
-    <div className="app-root-1">
-      <header className="Paper-root AppBar-root AppBar-positionStatic AppBar-colorPrimary Paper-elevation4">
-        <div className="Toolbar-root Toolbar-regular header-appBar-13 Toolbar-gutters">
-          <div className="header-left-4"></div>
-          <img className="header-logo-11" src={logo} />
-          <div className="header-vertical-9"></div>
-          <h5 className="Typography-root header-logo-text">OpenD</h5>
-          <div className="header-empty-6"></div>
-          <div className="header-space-8"></div>
-          <button className="ButtonBase-root Button-root Button-text header-navButtons-3">
-            Discover
-          </button>
-          <button className="ButtonBase-root Button-root Button-text header-navButtons-3">
-            Minter
-          </button>
-          <button className="ButtonBase-root Button-root Button-text header-navButtons-3">
-            My NFTs
-          </button>
-        </div>
-      </header>
-    </div>
+    <BrowserRouter forceRefresh={true}>
+      <div className="app-root-1">
+        <header className="Paper-root AppBar-root AppBar-positionStatic AppBar-colorPrimary Paper-elevation4">
+          <div className="Toolbar-root Toolbar-regular header-appBar-13 Toolbar-gutters">
+            <div className="header-left-4"></div>
+            <img className="header-logo-11" src={logo} />
+            <div className="header-vertical-9"></div>
+            <Link to="/">
+              <h5 className="Typography-root header-logo-text">OpenD</h5>
+            </Link>
+            <div className="header-empty-6"></div>
+            <div className="header-space-8"></div>
+            <button className="ButtonBase-root Button-root Button-text header-navButtons-3">
+              <Link to="/discover">Discover</Link>
+            </button>
+            <button className="ButtonBase-root Button-root Button-text header-navButtons-3">
+              <Link to="/minter">Minter</Link>
+            </button>
+            <button className="ButtonBase-root Button-root Button-text header-navButtons-3">
+              <Link to="/collection">My NFTs</Link>
+            </button>
+          </div>
+        </header>
+      </div>
+      <Switch>
+        <Route path="/"><img className="bottom-space" src={homeImage} /></Route>
+        <Route path="/discover"><h1>Discover</h1></Route>
+        <Route path="/minter"><Minter /></Route>
+        <Route path="/collection"><Gallery title="My NFTs" /></Route>
+      </Switch>
+    </BrowserRouter>
   );
+
 }
 
 export default Header;
